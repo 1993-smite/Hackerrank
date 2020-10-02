a5e6ff760e25e437a6fb47e5456a6869248369ea
Alex Sasov <sasov1993@mail.ru>
2020-10-02 11:10:37 +0300

    добавил проект для интегрирования и его тестов

---
+++ ConsoleApp2/Integral/RectangleIntegration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Integral
+{
+    public delegate double Fx(double x);
+
+    public class RectangleIntegration
+    {
+        public static double Integration(double from, double h, double to, Fx f)
+        {
+            double res = 0;
+            long count = 0;
+            for(double x = from; x < to; x += h)
+            {
+                count++;
+                res += f(x);
+            }
+            res *= (to - from) / count;
+            return res;
+        }
+
+
+        public delegate double FxBy(double x);
+        public double IntegrationBy(double from, double h, double to, FxBy f)
+        {
+            double res = 0;
+            long count = 0;
+            for (double x = from; x < to; x += h)
+            {
+                count++;
+                res += f(x);
+            }
+            res *= (to - from) / count;
+            return res;
+        }
+    }
+}
---
+++ ConsoleApp2/IntegralTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using Integral;
+using System;
+
+namespace IntegralTest
+{
+    class Program
+    {
+        public static double f(double x)
+        {
+            return x * x;
+        }
+
+        static void Main(string[] args)
+        {
+            double res = RectangleIntegration.Integration(0, 0.0001, 1, f);
+
+            Console.WriteLine(res);
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
