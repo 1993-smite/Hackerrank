4a825c1c64dcddb5a4eb9c71f4072de2282172cb
Alex Sasov <sasov1993@mail.ru>
2021-01-18 18:33:11 +0300

    2 новых задачи

---
+++ Hackerrank/DivisibleSumPairs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibleSumPairs
+{
+    class Program
+    {
+        // Complete the divisibleSumPairs function below.
+        static int divisibleSumPairs(int n, int k, int[] ar)
+        {
+            int count = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = i + 1; j < n; j++)
+                {
+                    if ((ar[i] + ar[j]) % k == 0)
+                        count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            string input = "1 3 2 6 1 2";
+            var arr = input.Split(' ').Select(x => int.Parse(x)).ToList();
+            int k = 3;
+            var res = divisibleSumPairs(arr.Count,k,arr.ToArray());
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hackerrank/DrawingBook/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingBook
+{
+    class Program
+    {
+        /*
+         * Complete the pageCount function below.
+         */
+        static int pageCount(int n, int p)
+        {
+            int count = 0;
+            var book = new List<Tuple<int, int>>();
+
+            for (int i = 0; i <= n; i+=2)
+                book.Add(new Tuple<int, int>(i, i + 1));
+
+            for(int i=0; i < book.Count; i++)
+            {
+                if (book[i].Item1 == p || book[i].Item2 == p)
+                {
+                    count = i;
+                    break;
+                }
+            }
+
+            for (int i = book.Count - 1; i > 0; i--)
+            {
+                if (book[i].Item1 == p || book[i].Item2 == p)
+                {
+                    if (book.Count - i < count)
+                        count = book.Count - i - 1;
+                    break;
+                }
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            var res = pageCount(5,4);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
