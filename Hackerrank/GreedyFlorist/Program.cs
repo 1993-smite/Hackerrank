8eb2142f325dcdf0ec41f3774168ed0dd4f2cefa
Alex Sasov <sasov1993@mail.ru>
2021-01-19 08:22:44 +0300

    задача хитрого флориста

---
+++ Hackerrank/GreedyFlorist/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedyFlorist
+{
+    class Program
+    {
+        // Complete the getMinimumCost function below.
+        static int getMinimumCost(int k, int[] c)
+        {
+            int result = 0;
+            var sortedBack = c.OrderByDescending(x=>x).ToArray();
+            int indexBuy = 0;
+            int count = c.Length;
+
+            for(int i = 0; i < count; i++)
+            {
+                result += sortedBack[i] * (1 + indexBuy / k);
+                indexBuy++;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string input = "2 5 6";
+            var arr = input.Split(' ').Select(x => int.Parse(x)).ToList();
+            int k = 3;
+            var res = getMinimumCost(k, arr.ToArray());
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
