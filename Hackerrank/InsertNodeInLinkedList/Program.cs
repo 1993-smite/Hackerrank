0d93c40a88dc6c1fd07682b072c6b5f150604c59
Alex Sasov <sasov1993@mail.ru>
2021-06-05 12:43:41 +0300

    task checked

---
+++ Hackerrank/InsertNodeInLinkedList/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace InsertNodeInLinkedList
+{
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    class Program
+    {
+        public static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position)
+        {
+            var node = llist;
+
+            var newNode = new SinglyLinkedListNode(data);
+
+            for(int i = 1; i < position; i++)
+            {
+                node = node.next;
+            }
+            newNode.next = node.next;
+            node.next = newNode;
+
+            return newNode;
+        }
+
+        static void Main(string[] args)
+        {
+            var massStr = "16,13,7".Split(",");
+
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            foreach (var it in massStr)
+            {
+                llist.InsertNode(int.Parse(it));
+            }
+
+            insertNodeAtPosition(llist.head, 1, 2);
+
+            Console.ReadLine();
+        }
+    }
+}
